51a382d75d106a8eb71c2f6bce5bed02932b9856
Maria Grigorova <mmgrigorova@gmail.com>
2022-03-10 11:19:42 +0200

    Remove unnecessary code

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
 const getSecretNumber = () => {
     return Math.trunc(Math.random() * 20) + 1;
 }
